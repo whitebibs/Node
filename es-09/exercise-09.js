9e6f362c61b2724870578b6deaf077b4654d62df
biancapatrichi <115084982+whitebibs@users.noreply.github.com>
2023-03-24 12:51:45 +0100

    Add files via upload

---
+++ es-09/exercise-09.js
@@ -0,0 +1,29 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+        const win = Boolean(Math.round(Math.random()));
+
+        process.nextTick(() => {
+            if (win) {
+                resolve(`${player} won a prize in the draw!`);
+            } else {
+                reject(new Error(`${player} lost the draw.`));
+            }
+        });
+    });
+}
+
+luckyDraw('Joe').
+    then((result) => {
+        console.log(result);
+        return luckyDraw('Caroline');
+    })
+    .then((result) => {
+        console.log(result);
+        return luckyDraw('Sabrina');
+    })
+    .then((then) => {
+        console.log(result);
+    })
+    .catch((er) => {
+        console.error(er)
+    })
