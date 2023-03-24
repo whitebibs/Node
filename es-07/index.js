a97e3912e0ce083dee001fa3009ad8460ac92181
biancapatrichi <115084982+whitebibs@users.noreply.github.com>
2023-03-24 13:21:30 +0100

    Add files via upload

---
+++ es-07/index.js
@@ -0,0 +1,16 @@
+var figlet = require('figlet');
+
+figlet.text('Ciao Persona!!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
